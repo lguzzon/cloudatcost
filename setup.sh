ac6f88060c5a5457202c5f8b697bf8408534567e
Luca Guzzon <luca.guzzon@gmail.com>
2016-12-10 17:10:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ if [[ -z $CACIP || -z $CACUSER ]]; then
   exit 1
 fi
 
-ssh root@$CACIP "curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/root.sh | bash -s \"$CACUSER\""
+ssh root@$CACIP "curl -s https://raw.githubusercontent.com/lguzzon/cloudatcost/master/root.sh | bash -s \"$CACUSER\""
 
 CACPWD=$(cat /proc/sys/kernel/random/uuid)
 echo $CACPWD
-curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/user_local.sh | bash -s $CACIP $CACUSER $CACPWD
+curl -s https://raw.githubusercontent.com/lguzzon/cloudatcost/master/user_local.sh | bash -s $CACIP $CACUSER $CACPWD
 
-ssh $CACUSER@$CACIP 'curl -s https://raw.githubusercontent.com/yulvil/cloudatcost/master/user.sh | bash -s'
+ssh $CACUSER@$CACIP 'curl -s https://raw.githubusercontent.com/lguzzon/cloudatcost/master/user.sh | bash -s'
